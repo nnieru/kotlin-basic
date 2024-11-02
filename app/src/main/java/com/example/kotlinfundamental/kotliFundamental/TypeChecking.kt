8b130514eaa46bfe5f024ec1d98ee2e0cbc44ed2
Nathaniel Andrian <nathaniel.andrian@binus.edu>
2024-11-03 01:09:08 +0700

    type checking

--- app/src/main/java/com/example/kotlinfundamental/kotliFundamental/TypeChecking.kt
+++ app/src/main/java/com/example/kotlinfundamental/kotliFundamental/TypeChecking.kt
@@ -5,6 +5,7 @@ fun getType (data: Any) {
      is Int -> println("Integer")
      is String -> println("String")
      is Boolean -> println("Boolean")
+     is TypeChecking -> println("This is Enum  Class of TypeChecking")
      else -> println("Unknown")
  }
 }
@@ -14,4 +15,24 @@ fun main() {
     getType("hello")
     getType(true)
     getType(1F)
+    getType(TypeChecking.TYPE_2)
+    castingExample()
+}
+
+enum class  TypeChecking {
+    TYPE_1, TYPE_2, TYPE_3
+}
+
+// casting -> merubah satu tipe data ke bentuk tipe data lain
+
+fun castingExample() {
+    val data = "1"
+    val number = data.toInt()
+    val input = "1000.100"
+    val doubleInput = input.toDouble()
+    val discount = 10.0
+
+    val total = doubleInput - (doubleInput * discount / 100)
+    println(number)
+    println(total)
 }
